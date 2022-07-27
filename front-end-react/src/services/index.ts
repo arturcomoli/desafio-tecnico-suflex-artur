9f992ff191de2bb489cc608264886bc4106c6634
Artur Comoli <artur.comoli@gmail.com>
2022-07-27 13:01:41 -0300

    fix: api url

--- front-end-react/src/services/index.ts
+++ front-end-react/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://app-desafio-suflex.herokuapp.com",
+  baseURL: "http://localhost:8000",
 });
 
 export const charApi = axios.create({
